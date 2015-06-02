2a521ce9327b83c22988cd7e67ea67eca1a235b1
Thomas Steur <thomas.steur@gmail.com>
2015-06-02 01:09:51 +0000

    added missing files

---
+++ plugins/CoreHome/Types/MobileApp.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\CoreHome\Types;
+
+use Piwik\Type\Type;
+use Piwik\Type\TypeSetting;
+use Piwik\Type\TypeSettings;
+
+class MobileApp extends Type
+{
+    const ID = 'mobileapp';
+    protected $name = 'General_MobileApp';
+
+    public function configureSettings(TypeSettings $settings)
+    {
+        $settings->addSetting(new TypeSetting('AppId', 'AppId'));
+    }
+}
+
---
+++ plugins/CoreHome/Types/Website.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\CoreHome\Types;
+
+use Piwik\Type\Type;
+use Piwik\Type\TypeSetting;
+use Piwik\Type\TypeSettings;
+
+class Website extends Type
+{
+    const ID = 'website';
+    protected $name = 'General_Website';
+
+    public function configureSettings(TypeSettings $settings)
+    {
+        $settings->addSetting(new TypeSetting('URLs', 'URLs'));
+    }
+}
+
